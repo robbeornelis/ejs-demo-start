58c1a0f6439e1286bbaa827c0e791716893afed2
robbeornelis <robbe.ornelis@student.arteveldehs.be>
2025-02-18 10:33:03 +0100

    added pages map and changed their location

--- src/controllers/PageController.js
+++ src/controllers/PageController.js
@@ -1,26 +1,26 @@
 export const home = (req, res) => {
-  res.render("home", {
+  res.render("pages/home", {
     title: "dinosaurs around the world",
     message: "Welcome to the world of dinosaurs",
   });
 };
 
 export const about = (req, res) => {
-  res.render("about", {
-    title: "about us",
-    message: "we're a group of dinosaur enthusiasts",
+  res.render("pages/default", {
+    title: "About us",
+    message: "We're a group of dinosaur enthusiasts!",
   });
 };
 
 export const contact = (req, res) => {
-  res.render("contact", {
-    title: "contact",
-    message: "contact us at 09 000 00 00",
+  res.render("pages/contact", {
+    title: "Contact",
+    message: "Contact us at 09 000 00 00",
   });
 };
 
 export const privacy = (req, res) => {
-  res.render("privacy", {
+  res.render("pages/default", {
     title: "Privacy Police",
     message: "Dinosaurs are protective of their privacy",
   });
