049272f4ec8b7051d1330a36ec44b1cf9554353d
robbeornelis <robbe.ornelis@student.arteveldehs.be>
2025-02-18 09:33:29 +0100

    commit 2

--- src/app.js
+++ src/app.js
@@ -5,13 +5,20 @@ import path from "path";
 // create an instance of express
 const app = express();
 
+//set the view engine to ejs
+app.set("view engine", "ejs");
+app.set("views", path.resolve("src", "views"));
+
 // serve static files from the public folder
 // they can be accessed from the root of the site (e.g. /assets/images/dino_07.png) 🦕
 app.use(express.static("public"));
 
-// GET route to serve the index.html file
+// GET route to serve the home.ejs file
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("src", "views", "index.html"));
+  res.render("home", {
+    title: "Dinosaurs are awesome!",
+    content: "Dinosaurs are a diverse group of reptiles of the clade Dinosaur",
+  });
 });
 
 // start the server, listen on port defined in .env file
